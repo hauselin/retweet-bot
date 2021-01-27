e84d541ed8669a228e7d0285333ef3c8c27321ea
hauselin <hauselin@gmail.com>
2021-01-27 15:46:37 -0600

    add requirements

--- retweet_bot.py
+++ retweet_bot.py
@@ -17,4 +17,4 @@ ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 ACCESS_SECRET = os.getenv("ACCESS_SECRET")
 
 api = tweepy_auth(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
-retweet_favorites(api, "hauselin", count=3)
+retweet_favorites(api, "hauselin", count=10)
